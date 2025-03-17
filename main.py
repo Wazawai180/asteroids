f1ea1746d71e356c927a0827a5c8533e62802828
Wazawai180 <wazawai180@gmail.com>
2025-03-17 16:23:14 -0700

    implemented player sprite and l-r controls

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import pygame
-from constants import *
+import constants
+from player import Player
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill("black")
-        pygame.display.flip()
-
         dt = clock.tick(60) / 1000
+            
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
